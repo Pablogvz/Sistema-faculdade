cd7097477ec1ac65a89783ddafff40ef7f5decc2
Pablo Martinez <Pablo Martinez@DELLG15>
2024-05-15 21:52:34 -0400

    Adicionar arquivos de projeto.

---
+++ Enums/TipoCurso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapiens.Shared.Enums
+{
+    public enum TipoCurso
+    {
+        Tecnologo,
+        Licenciatura,
+        Bacharelado,
+        Especializacao,
+        Mestrado,
+        Doutorado
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Sapiens.Shared.Entities;
+using Sapiens.Shared.Enums;
+
+var curso = new Curso("Sistemas da informação");
+Console.WriteLine(curso.ToString());
