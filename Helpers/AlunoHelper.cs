802435e05268379bce99f2fd7e55102c7e74229c
Pablo Martinez <pablogvozdanovic@gmail.com>
2024-06-16 22:12:53 -0400

    Adicionando Enums nos helpers

--- Helpers/AlunoHelper.cs
+++ Helpers/AlunoHelper.cs
@@ -1,134 +1,150 @@
 ﻿using Sapiens.Shared.Entities;
+using Sapiens.Shared.Enums; // Ensure this includes TipoSexo
 
-namespace Sapiens.Shared.Helpers;
-
-public static partial class ConsoleHelper
+namespace Sapiens.Shared.Helpers
 {
-    public static void MenuAluno()
+    public static partial class ConsoleHelper
     {
-        CriarTitulo("Sapiens - Cadastro de Alunos");
-        var opcao = "";
-        Console.WriteLine(" [1] Listar");
-        Console.WriteLine(" [2] Consultar");
-        Console.WriteLine(" [3] Adicionar");
-        Console.WriteLine(" [4] Editar");
-        Console.WriteLine(" [5] Remover");
-        Console.WriteLine("\n [9] Voltar");
-        Console.WriteLine("-----------------------------------");
-        Console.Write(" \nOpção: ");
-        opcao = Console.ReadLine();
-        switch (opcao)
+        public static void MenuAluno()
         {
-            case "1": ListarAlunos(); break;
-            case "2": ConsultarAlunos(); break;
-            case "3": AdicionarAlunos(); break;
-            case "4": EditarAlunos(); break;
-            case "5": RemoverAlunos(); break;
-            case "9": Menu(); break;
+            CriarTitulo("Sapiens - Cadastro de Alunos");
+            var opcao = "";
+            Console.WriteLine(" [1] Listar");
+            Console.WriteLine(" [2] Consultar");
+            Console.WriteLine(" [3] Adicionar");
+            Console.WriteLine(" [4] Editar");
+            Console.WriteLine(" [5] Remover");
+            Console.WriteLine("\n [9] Voltar");
+            Console.WriteLine("-----------------------------------");
+            Console.Write(" \nOpção: ");
+            opcao = Console.ReadLine();
+            switch (opcao)
+            {
+                case "1": ListarAlunos(); break;
+                case "2": ConsultarAlunos(); break;
+                case "3": AdicionarAlunos(); break;
+                case "4": EditarAlunos(); break;
+                case "5": RemoverAlunos(); break;
+                case "9": Menu(); break;
+            }
         }
-    }
 
-
-    public static void ListarAcaoAlunos()
-    {
-        CriarTitulo("Sapiens - Lista de Alunos");
-        var alunos = context.Alunos.OrderBy(a => a.Nome).ToList();
-        foreach (var aluno in alunos)
+        public static void ListarAcaoAlunos()
         {
-            Console.WriteLine($"Aluno(a): {aluno.Cpf} - {aluno.Nome}");
+            CriarTitulo("Sapiens - Lista de Alunos");
+            var alunos = context.Alunos.OrderBy(a => a.Nome).ToList();
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine($"Aluno(a): {aluno.Cpf} - {aluno.Nome}");
+            }
         }
-    }
 
-    public static void ListarAlunos()
-    {
-        CriarTitulo("Sapiens - Lista de Alunos");
-        var alunos = context.Alunos.OrderBy(a => a.Nome).ToList();
-        foreach (var aluno in alunos)
+        public static void ListarAlunos()
         {
-            Console.WriteLine($"{aluno.Cpf} - {aluno.Nome}");
+            CriarTitulo("Sapiens - Lista de Alunos");
+            var alunos = context.Alunos.OrderBy(a => a.Nome).ToList();
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine($"{aluno.Cpf} - {aluno.Nome}, Sexo: {aluno.Sexo}");
+            }
+            EnterParaContinuar("-----------------------------------");
+            MenuAluno();
         }
-        EnterParaContinuar("-----------------------------------");
-        MenuAluno();
-    }
 
-    public static void ConsultarAlunos()
-    {
-        CriarTitulo("Sapiens - Consultar Aluno");
-        var termo = LeiaTexto("Pesquise por CPF ou Nome");
-        termo = termo.ToLower();
-        var alunos = context.Alunos
-            .Where(a => (a.Nome.ToLower().Contains(termo)) || a.Cpf.Contains(termo))
-            .OrderBy(a => a.Nome)
-            .ToList();
-        foreach (var aluno in alunos)
+        public static void ConsultarAlunos()
         {
-            Console.WriteLine($"{aluno.Nome} ({aluno.Cpf})");
+            CriarTitulo("Sapiens - Consultar Aluno");
+            var termo = LeiaTexto("Pesquise por CPF ou Nome");
+            termo = termo.ToLower();
+            var alunos = context.Alunos
+                .Where(a => (a.Nome.ToLower().Contains(termo)) || a.Cpf.Contains(termo))
+                .OrderBy(a => a.Nome)
+                .ToList();
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine($"{aluno.Nome} ({aluno.Cpf}), Sexo: {aluno.Sexo}");
+            }
+            EnterParaContinuar("-----------------------------------");
+            MenuAluno();
         }
-        EnterParaContinuar("-----------------------------------");
-        MenuAluno();
-    }
 
-    public static void AdicionarAlunos()
-    {
-        CriarTitulo("Sapiens - Adicionar Aluno");
-        var nome = LeiaTexto("Nome do Aluno");
-        var cpf = LeiaTexto("Cpf");
-
-        var aluno = new Aluno()
+        public static void AdicionarAlunos()
         {
-            Nome = nome,
-            Cpf = cpf
-        };
-        context.Add(aluno);
-        context.SaveChanges();
-        EnterParaContinuar("-----------------------------------\nAluno Adicionado");
-        MenuAluno();
-    }
+            CriarTitulo("Sapiens - Adicionar Aluno");
+            var nome = LeiaTexto("Nome do Aluno");
+            var cpf = LeiaTexto("Cpf");
+            var sexo = SelecionarTipoSexo();
 
-    public static void EditarAlunos()
-    {
-        CriarTitulo("Sapiens - Editar Aluno");
-        ListarAcaoAlunos();
-        var cpf = LeiaTexto("Cpf do Aluno a Editar");
-        var aluno = context.Alunos.FirstOrDefault(a => a.Cpf == cpf);
-        if (aluno != null)
-        {
-            var novoNome = LeiaTexto($"Novo Nome do Aluno (Atual: {aluno.Nome})");
-            aluno.Nome = string.IsNullOrEmpty(novoNome) ? aluno.Nome : novoNome;
-
-            var novoCpf = LeiaTexto($"Novo Cpf do Aluno (Atual: {aluno.Cpf})");
-            aluno.Cpf = string.IsNullOrEmpty(novoCpf) ? aluno.Cpf : novoCpf;
-
-            context.Update(aluno);
+            var aluno = new Aluno()
+            {
+                Nome = nome,
+                Cpf = cpf,
+                Sexo = sexo
+            };
+            context.Add(aluno);
             context.SaveChanges();
-            Console.WriteLine("Aluno atualizado.");
+            EnterParaContinuar("-----------------------------------\nAluno Adicionado");
+            MenuAluno();
         }
-        else
-        {
-            Console.WriteLine("Aluno inexistente.");
-        }
-        EnterParaContinuar("-----------------------------------");
-        MenuAluno();
-    }
 
-    public static void RemoverAlunos()
-    {
-        CriarTitulo("Sapiens - Remover Aluno");
-        ListarAcaoAlunos();
-        var cpf = LeiaTexto("Cpf do Aluno");
-        var aluno = context.Alunos.FirstOrDefault(a => a.Cpf == cpf);
-        if (aluno != null)
+        public static void EditarAlunos()
         {
-            context.Remove(aluno);
-            context.SaveChanges();
-            Console.WriteLine("Aluno excluído.");
-        }
-        else
-        {
-            Console.WriteLine("Aluno inexistente.");
-        }
-        EnterParaContinuar("-----------------------------------");
-        MenuAluno();
-    }
+            CriarTitulo("Sapiens - Editar Aluno");
+            ListarAcaoAlunos();
+            var cpf = LeiaTexto("Cpf do Aluno a Editar");
+            var aluno = context.Alunos.FirstOrDefault(a => a.Cpf == cpf);
+            if (aluno != null)
+            {
+                var novoNome = LeiaTexto($"Novo Nome do Aluno (Atual: {aluno.Nome})");
+                aluno.Nome = string.IsNullOrEmpty(novoNome) ? aluno.Nome : novoNome;
 
+                var novoCpf = LeiaTexto($"Novo Cpf do Aluno (Atual: {aluno.Cpf})");
+                aluno.Cpf = string.IsNullOrEmpty(novoCpf) ? aluno.Cpf : novoCpf;
+
+                var sexo = SelecionarTipoSexo();
+                aluno.Sexo = sexo;
+
+                context.Update(aluno);
+                context.SaveChanges();
+                Console.WriteLine("Aluno atualizado.");
+            }
+            else
+            {
+                Console.WriteLine("Aluno inexistente.");
+            }
+            EnterParaContinuar("-----------------------------------");
+            MenuAluno();
+        }
+
+        public static void RemoverAlunos()
+        {
+            CriarTitulo("Sapiens - Remover Aluno");
+            ListarAcaoAlunos();
+            var cpf = LeiaTexto("Cpf do Aluno");
+            var aluno = context.Alunos.FirstOrDefault(a => a.Cpf == cpf);
+            if (aluno != null)
+            {
+                context.Remove(aluno);
+                context.SaveChanges();
+                Console.WriteLine("Aluno excluído.");
+            }
+            else
+            {
+                Console.WriteLine("Aluno inexistente.");
+            }
+            EnterParaContinuar("-----------------------------------");
+            MenuAluno();
+        }
+
+        private static TipoSexo SelecionarTipoSexo()
+        {
+            Console.WriteLine("Selecione o sexo do aluno:");
+            foreach (var tipo in Enum.GetValues(typeof(TipoSexo)))
+            {
+                Console.WriteLine($" [{(int)tipo}] {tipo}");
+            }
+            var opcao = LeiaInteiro("Opção");
+            return (TipoSexo)opcao;
+        }
+    }
 }
--- Helpers/CursoHelper.cs
+++ Helpers/CursoHelper.cs
@@ -1,124 +1,100 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sapiens.Shared.Entities;
+using Sapiens.Shared.Enums;
+using System;
+using System.Linq;
 
-namespace Sapiens.Shared.Helpers;
-
-public static partial class ConsoleHelper
+namespace Sapiens.Shared.Helpers
 {
-    public static void MenuCurso()
+    public static partial class ConsoleHelper
     {
-        CriarTitulo("Sapiens - Cadastro de Cursos");
-        var opcao = "";
-        Console.WriteLine(" [1] Listar");
-        Console.WriteLine(" [2] Consultar");
-        Console.WriteLine(" [3] Adicionar");
-        Console.WriteLine(" [4] Editar");
-        Console.WriteLine(" [5] Remover");
-        Console.WriteLine("\n [9] Voltar");
-        Console.WriteLine("-----------------------------------");
-        Console.Write(" \nOpção: ");
-        opcao = Console.ReadLine();
-        switch (opcao)
+        public static void MenuCurso()
         {
-            case "1": ListarCursos(); break;
-            case "2": ConsultarCurso(); break;
-            case "3": AdicionarCurso(); break;
-            case "4": EditarCurso(); break;
-            case "5": RemoverCurso(); break;
-            case "9": Menu(); break;
+            CriarTitulo("Sapiens - Cadastro de Cursos");
+            var opcao = "";
+            Console.WriteLine(" [1] Listar");
+            Console.WriteLine(" [2] Consultar");
+            Console.WriteLine(" [3] Adicionar");
+            Console.WriteLine(" [4] Editar");
+            Console.WriteLine(" [5] Remover");
+            Console.WriteLine("\n [9] Voltar");
+            Console.WriteLine("-----------------------------------");
+            Console.Write(" \nOpção: ");
+            opcao = Console.ReadLine();
+            switch (opcao)
+            {
+                case "1": ListarCursos(); break;
+                case "2": ConsultarCurso(); break;
+                case "3": AdicionarCurso(); break;
+                case "4": EditarCurso(); break;
+                case "5": RemoverCurso(); break;
+                case "9": Menu(); break;
+            }
         }
-    }
 
-    public static void ListarCursos()
-    {
-        CriarTitulo("Sapiens - Lista de Cursos");
-        var cursos = context.Cursos.Include(c => c.Coordenador).OrderBy(c => c.Nome).ToList();
-        foreach (var curso in cursos)
+        public static void ListarCursos()
         {
-            Console.WriteLine($"ID: {curso.Id}. Curso: {curso.Nome}. Coordenador: {curso?.Coordenador?.Nome} ");
+            CriarTitulo("Sapiens - Lista de Cursos");
+            var cursos = context.Cursos.Include(c => c.Coordenador).OrderBy(c => c.Nome).ToList();
+            foreach (var curso in cursos)
+            {
+                var coordenadorNome = curso.Coordenador?.Nome ?? "N/A";
+                Console.WriteLine($"ID: {curso.Id}. Curso: {curso.Nome}. Coordenador: {coordenadorNome}, Tipo: {curso.Tipo}");
+            }
+            EnterParaContinuar("-----------------------------------");
+            MenuCurso();
         }
-        EnterParaContinuar("-----------------------------------");
-        MenuCurso();
-    }
 
-    public static void ListarAcaoCursos()
-    {
-        CriarTitulo("Sapiens - Lista de Cursos");
-        var cursos = context.Cursos.Include(c => c.Coordenador).OrderBy(c => c.Nome).ToList();
-        foreach (var curso in cursos)
+        public static void ListarAcaoCursos()
         {
-            Console.WriteLine($"ID: {curso.Id}. Curso: {curso.Nome}. Coordenador: {curso?.Coordenador?.Nome} ");
+            CriarTitulo("Sapiens - Lista de Cursos");
+            var cursos = context.Cursos.Include(c => c.Coordenador).OrderBy(c => c.Nome).ToList();
+            foreach (var curso in cursos)
+            {
+                var coordenadorNome = curso.Coordenador?.Nome ?? "N/A";
+                Console.WriteLine($"ID: {curso.Id}. Curso: {curso.Nome}. Coordenador: {coordenadorNome}, Tipo: {curso.Tipo}");
+            }
+            Console.WriteLine("-----------------------------------");
         }
-        Console.WriteLine("-----------------------------------");
-    }
 
-    public static void ListarCoordenadores()
-    {
-        CriarTitulo("Sapiens - Lista de Coordenadores");
-        var coordenadores = context.Professores.OrderBy(p => p.Nome).ToList();
-        foreach (var coordenador in coordenadores)
+        public static void ListarCoordenadores()
         {
-            Console.WriteLine($"CPF: {coordenador.Cpf} - Nome: {coordenador.Nome}");
+            CriarTitulo("Sapiens - Lista de Coordenadores");
+            var coordenadores = context.Professores.OrderBy(p => p.Nome).ToList();
+            foreach (var coordenador in coordenadores)
+            {
+                Console.WriteLine($"CPF: {coordenador.Cpf} - Nome: {coordenador.Nome}");
+            }
+            Console.WriteLine("-----------------------------------");
         }
-        Console.WriteLine("-----------------------------------");
-    }
 
-    public static void ConsultarCurso()
-    {
-        CriarTitulo("Sapiens - Consultar Curso");
-        var termo = LeiaTexto("Pesquise por Nome do Curso");
-        termo = termo.ToLower();
-        var cursos = context.Cursos.Where(c => c.Nome != null && c.Nome.ToLower().Contains(termo)).ToList();
-        foreach (var curso in cursos)
+        public static void ConsultarCurso()
         {
-            Console.WriteLine($"{curso.Nome} ({curso.CargaHoraria}h)");
+            CriarTitulo("Sapiens - Consultar Curso");
+            var termo = LeiaTexto("Pesquise por Nome do Curso");
+            termo = termo.ToLower();
+            var cursos = context.Cursos.Where(c => c.Nome != null && c.Nome.ToLower().Contains(termo)).ToList();
+            foreach (var curso in cursos)
+            {
+                Console.WriteLine($"ID: {curso.Id}. Curso: {curso.Nome} ({curso.CargaHoraria}h), Tipo: {curso.Tipo}");
+            }
+            EnterParaContinuar("-----------------------------------");
+            MenuCurso();
         }
-        EnterParaContinuar("-----------------------------------");
-        MenuCurso();
-    }
 
-    public static void AdicionarCurso()
-    {
-        CriarTitulo("Sapiens - Adicionar Curso");
-        var nome = LeiaTexto("Nome do Curso");
-        var ch = LeiaInteiro("Carga Horária");
-        var curso = new Curso()
+        public static void AdicionarCurso()
         {
-            Nome = nome,
-            CargaHoraria = ch
-        };
+            CriarTitulo("Sapiens - Adicionar Curso");
+            var nome = LeiaTexto("Nome do Curso");
+            var ch = LeiaInteiro("Carga Horária");
+            var tipo = SelecionarTipoCurso();
 
-        ListarCoordenadores();
-
-        var cpf = LeiaTexto("Cpf do Coordenador");
-        if (cpf != "")
-        {
-            var coordenador = context.Professores.FirstOrDefault(p => p.Cpf == cpf);
-            if (coordenador != null)
-                curso.Coordenador = coordenador;
-            else
-                Console.WriteLine("Professor inexistente, coordenador não atribuído.");
-        }
-        context.Add(curso);
-        context.SaveChanges();
-        EnterParaContinuar("-----------------------------------\nCurso Adicionado");
-        MenuCurso();
-    }
-
-    public static void EditarCurso()
-    {
-        CriarTitulo("Sapiens - Editar Curso");
-        ListarAcaoCursos();
-        var id = LeiaInteiro("Id do Curso");
-        var curso = context.Cursos.Include(c => c.Coordenador).FirstOrDefault(c => c.Id == id);
-        if (curso != null)
-        {
-            var nome = LeiaTexto($"Nome do Curso ({curso.Nome})");
-            if (!string.IsNullOrEmpty(nome))
-                curso.Nome = nome;
-
-            var ch = LeiaInteiro($"Carga Horária ({curso.CargaHoraria})");
-            curso.CargaHoraria = ch;
+            var curso = new Curso()
+            {
+                Nome = nome,
+                CargaHoraria = ch,
+                Tipo = tipo
+            };
 
             ListarCoordenadores();
             var cpf = LeiaTexto("Cpf do Coordenador");
@@ -130,36 +106,82 @@ public static partial class ConsoleHelper
                 else
                     Console.WriteLine("Professor inexistente, coordenador não atribuído.");
             }
-
-            context.Update(curso);
+            context.Add(curso);
             context.SaveChanges();
-            EnterParaContinuar("-----------------------------------\nCurso Atualizado");
+            EnterParaContinuar("-----------------------------------\nCurso Adicionado");
+            MenuCurso();
         }
-        else
-        {
-            Console.WriteLine("Curso inexistente.");
-        }
-        MenuCurso();
-    }
 
-    public static void RemoverCurso()
-    {
-        CriarTitulo("Sapiens - Remover Curso");
-        ListarAcaoCursos();
-        var id = LeiaInteiro("Id do Curso");
-        var curso = context.Cursos.Find(id);
-        if (curso != null)
+        public static void EditarCurso()
         {
-            context.Remove(curso);
-            context.SaveChanges();
-            Console.WriteLine("Curso excluído.");
-        }
-        else
-        {
-            Console.WriteLine("Curso inexistente.");
-        }
-        EnterParaContinuar("-----------------------------------");
-        MenuCurso();
-    }
+            CriarTitulo("Sapiens - Editar Curso");
+            ListarAcaoCursos();
+            var id = LeiaInteiro("Id do Curso a ser editado");
+            var curso = context.Cursos.Include(c => c.Coordenador).FirstOrDefault(c => c.Id == id);
+            if (curso != null)
+            {
+                Console.WriteLine("Deixe o campo em branco para manter o valor atual.");
+                var nome = LeiaTexto($"Nome do Curso ({curso.Nome})");
+                if (!string.IsNullOrEmpty(nome))
+                    curso.Nome = nome;
 
+                var ch = LeiaInteiro($"Carga Horária ({curso.CargaHoraria})");
+                curso.CargaHoraria = ch;
+
+                var tipo = SelecionarTipoCurso();
+                curso.Tipo = tipo;
+
+                ListarCoordenadores();
+                var cpf = LeiaTexto($"Cpf do Coordenador ({curso.Coordenador?.Cpf ?? "N/A"})");
+                if (!string.IsNullOrEmpty(cpf))
+                {
+                    var coordenador = context.Professores.FirstOrDefault(p => p.Cpf == cpf);
+                    if (coordenador != null)
+                        curso.Coordenador = coordenador;
+                    else
+                        Console.WriteLine("Professor inexistente, coordenador não atribuído.");
+                }
+
+                context.Update(curso);
+                context.SaveChanges();
+                EnterParaContinuar("-----------------------------------\nCurso Editado");
+            }
+            else
+            {
+                Console.WriteLine("Curso inexistente.");
+            }
+            MenuCurso();
+        }
+
+        public static void RemoverCurso()
+        {
+            CriarTitulo("Sapiens - Remover Curso");
+            ListarAcaoCursos();
+            var id = LeiaInteiro("Id do Curso");
+            var curso = context.Cursos.Find(id);
+            if (curso != null)
+            {
+                context.Remove(curso);
+                context.SaveChanges();
+                Console.WriteLine("Curso excluído.");
+            }
+            else
+            {
+                Console.WriteLine("Curso inexistente.");
+            }
+            EnterParaContinuar("-----------------------------------");
+            MenuCurso();
+        }
+
+        private static TipoCurso SelecionarTipoCurso()
+        {
+            Console.WriteLine("Selecione o tipo do curso:");
+            foreach (var tipo in Enum.GetValues(typeof(TipoCurso)))
+            {
+                Console.WriteLine($" [{(int)tipo}] {tipo}");
+            }
+            var opcao = LeiaInteiro("Opção");
+            return (TipoCurso)opcao;
+        }
+    }
 }
